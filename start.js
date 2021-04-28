ae35d05d858c142c7691bf7deca008e931b4d77d
Matt Jones <mattjones701@gmail.com>
2021-04-29 09:22:41 +1000

    first commit

---
+++ start.js
@@ -0,0 +1,47 @@
+import path from 'path';
+import { existsSync, promises as fs } from 'fs';
+
+import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
+import { serve } from 'esbuild';
+
+const entry = './src/index.ts';
+const outdir = path.join(process.cwd(), 'dist');
+
+async function run() {
+  if (existsSync(outdir)) {
+    await fs.rm(outdir, { recursive: true });
+  }
+
+  await fs.mkdir(outdir);
+
+  await serve(
+    { servedir: outdir, port: 8080 },
+    {
+      entryPoints: [entry],
+      platform: 'browser',
+      bundle: true,
+      plugins: [vanillaExtractPlugin()],
+      outdir,
+    },
+  );
+
+  await fs.writeFile(
+    path.join(outdir, 'index.html'),
+    `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>vanilla-extract-esbuild</title>
+        <link rel="stylesheet" type="text/css" href="index.css" />
+        </head>
+      <body>
+        <script src="index.js"></script>
+      </body>
+      </html>
+    `,
+  );
+  console.log('http://localhost:8080');
+}
+
+run();
